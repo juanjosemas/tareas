0bd71f7cb180c126c322293f6fcc772c08182c87
juanjosemas <48967497+juanjosemas@users.noreply.github.com>
2025-06-18 13:33:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ function palabraANumero(palabraNumero) {
         'cinco': '5', 'seis': '6', 'siete': '7', 'ocho': '8', 'nueve': '9',
         'diez': '10', 'once': '11', 'doce': '12', 'trece': '13', 'catorce': '14',
         'quince': '15', 'dieciséis': '16', 'diecisiete': '17', 'dieciocho': '18',
-        'diecinueve': '19', 'veinte': '20'
+        'diecinueve': '19', 'veinte': '20', 'veintiuno': '21', 'veintidos': '22', 
+        'veintitres': '23', 'veinticuatro': '24', 'veinticinco': '25', 'veintiseis': '26',
+        'veintisiete': '27', 'veintiocho': '28', 'veintinueve': '29', 'treinta': '30',
         // Puedes expandir esta lista si necesitas más números
     };
     const numero = mapaNumeros[palabraLimpia];
